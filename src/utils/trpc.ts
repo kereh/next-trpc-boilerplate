c58edc668c99928f734b6c83ab1fa20637dfce75
kereh <kerehchannel@gmail.com>
2022-12-12 08:36:24 +0800

    fix trpc hooks

--- src/utils/trpc.ts
+++ src/utils/trpc.ts
@@ -3,7 +3,7 @@ import { createTRPCNext } from "@trpc/next";
 import type { App } from "../backend/routers/main";
 
 function url() {
-  if (typeof window == "undefined") return "";
+  if (typeof window !== "undefined") return "";
   if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`;
   return `http://localhost:${process.env.PORT ?? 3000}`;
 }
